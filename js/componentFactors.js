d80b04d60e7eeea81f1756a46bca80cfea072261
lucasekstrom <lucas.ekstrom@ki.se>
2025-03-23 21:17:02 +0100

    Esthetics

--- js/componentFactors.js
+++ js/componentFactors.js
@@ -62,41 +62,6 @@ function createRbcComponentFactorsContent() {
             </div>
           </div>
         </div>
-        
-        <div class="available-options">
-          <div class="available-options-toggle">Available Parameters & Factors <span class="toggle-icon">▼</span></div>
-          <div class="available-options-content">
-            <div class="row">
-              <div class="col">
-                <p><strong>Vital Parameters:</strong></p>
-                <ul class="compact-list">
-                  <li>Mean Arterial Pressure (mmHg)</li>
-                  <li>Systolic Blood Pressure (mmHg)</li>
-                  <li>Diastolic Blood Pressure (mmHg)</li>
-                  <li>Heart Rate (bpm)</li>
-                  <li>Fraction of Inspired Oxygen (%)</li>
-                  <li>Peripheral Capillary Oxygen Saturation (%)</li>
-                  <li>Minute Ventilation (L/min)</li>
-                </ul>
-              </div>
-              <div class="col">
-                <p><strong>RBC Component Factors:</strong></p>
-                <ul class="compact-list">
-                  <li>Donor Hemoglobin (g/L)</li>
-                  <li>Donor Parity (parous or nulliparous)</li>
-                  <li>Donor Sex (male or female)</li>
-                  <li>RBC Component Storage Time (days)</li>
-                  <li>Weekday of Donation</li>
-                </ul>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <div class="form-group compact">
-        <label class="form-label">Comparison Values:</label>
-        <div id="comparison-tags" class="comparison-tags-container"></div>
       </div>
       
       <div id="debug-container" style="display: none;">
@@ -136,21 +101,6 @@ function initializeRbcComponentFactors(containerId, fileCase, logDebug) {
   
   // This tab now uses the existing visualization functionality
   // (The functions from visualization.js already work correctly for this tab)
-  
-  // Setup toggle functionality for Available Parameters & Factors section
-  setTimeout(() => {
-    const availableOptionsToggle = container.querySelector('.available-options-toggle');
-    const availableOptions = container.querySelector('.available-options');
-    
-    if (availableOptionsToggle && availableOptions) {
-      availableOptionsToggle.addEventListener('click', () => {
-        availableOptions.classList.toggle('expanded');
-      });
-      
-      // Initially collapsed
-      availableOptions.classList.remove('expanded');
-    }
-  }, 100);
   
   logDebug('RBC Component Factors tab initialized');
 }
